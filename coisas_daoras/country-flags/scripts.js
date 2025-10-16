e3e91271796d22dffab334b16a38ffdeab00cccc
Arttur Souzza <souzzaaturr@gmail.com>
2025-10-15 22:42:50 -0300

    Altera estrutura for para função .forEach()
    Substitui a estrutura de repetição 'for' pela função '.forEach()', aderindo à programação funcional.

--- coisas_daoras/country-flags/scripts.js
+++ coisas_daoras/country-flags/scripts.js
@@ -4,15 +4,15 @@ fetch("bandeiras/countries.json")
       
         const sigla_paises = Object.keys(paises);
         const nome_paises = Object.values(paises);
-        const qtd_paises = sigla_paises.length;
+        const qtd_paises = sigla_paises.map((element, num = 0) => num++);
         
         let conteudo_principal = document.querySelector("#conteudo");
 
-        for (let i = 0; i < qtd_paises; i++) {
+        qtd_paises.forEach((posicao) => {
             conteudo_principal.innerHTML += 
             '<div id = "bloco-imagem">' +
-                '<img src = "bandeiras/svg/' + sigla_paises[i].toLowerCase() + '.svg" alt = "imagem-de-nacao" width = 200px">' + 
-                '<h3 id = "nome-pais">' + nome_paises[i] + '</h3>' +
+                '<img src = "bandeiras/svg/' + sigla_paises[posicao].toLowerCase() + '.svg" alt = "imagem-de-nacao" width = 200px">' + 
+                '<h3 id = "nome-pais">' + nome_paises[posicao] + '</h3>' +
             '</div>'
-        }
+        });
     })
