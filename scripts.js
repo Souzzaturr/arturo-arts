c0f91d1d840c6e15d8b7e97768ab4f4d928f33e4
Arttur Souzza <souzzaaturr@gmail.com>
2025-10-15 21:12:30 -0300

    Adiciona scripts
    Adiciona arquivo de scripts e tag linkando o html ao arquivo de scrits

---
+++ scripts.js
@@ -0,0 +1,20 @@
+fetch("bandeiras/countries.json")
+    .then(response => response.json())
+    .then(paises => {
+      
+        const sigla_paises = Object.keys(paises);
+        const nome_paises = Object.values(paises);
+        const qtd_paises = sigla_paises.length;
+        
+        let conteudo_principal = document.querySelector("#conteudo");
+
+        console.log(sigla_paises[2].toLowerCase() );
+
+        for (let i = 0; i < qtd_paises; i++) {
+            conteudo_principal.innerHTML += 
+            '<div id = "bloco-imagem">' +
+                '<img src = "bandeiras/svg/' + sigla_paises[i].toLowerCase() + '.svg" alt = "imagem-de-nacao" width = 200px">' + 
+                '<h3 id = "nome-pais">' + nome_paises[i] + '</h3>' +
+            '</div>'
+        }
+    })
